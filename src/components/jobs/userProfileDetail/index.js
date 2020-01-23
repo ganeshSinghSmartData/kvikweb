e312e598dc6e77bc1d819b9be261791ecd8a0ab4
Rajvir Singh <rajvir.singh@smartData-204.local>
2020-01-23 15:37:11 +0530

    ui feedback phase1

--- src/components/jobs/userProfileDetail/index.js
+++ src/components/jobs/userProfileDetail/index.js
@@ -21,7 +21,36 @@ const UserProfile = ({
 }) => {
   return (
     <div className="job-detail-blc d-flex flex-column flex-fill">
-      <Heading className="h3 text-center">Profile</Heading>
+      <div className="detail-blc-hd d-flex align-items-center mb-3">
+        <Heading className="h3 flex-fill mb-0 mr-3">
+          Profile
+        </Heading>
+        <div className="user-profl-edit">
+          <Button
+            color="link"
+            className="edit-btn"
+            onClick={() => _toggleEdit(!isEdit)}
+          >
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="19.446"
+                height="19.435"
+                viewBox="0 0 19.446 19.435"
+              >
+                <path
+                  id="edit"
+                  d="M18.987,1.513a4.29,4.29,0,0,0-6.07,0L1.788,12.633a.556.556,0,0,0-.157.319l-.825,6.1a.555.555,0,0,0,.157.468.566.566,0,0,0,.394.166.452.452,0,0,0,.075,0l3.679-.5a.56.56,0,1,0-.149-1.11l-2.95.4.576-4.258L7.071,18.7a.566.566,0,0,0,.394.166.549.549,0,0,0,.394-.166L18.987,7.58a4.283,4.283,0,0,0,0-6.067ZM13.132,2.88,15,4.748,4.846,14.9,2.977,13.031ZM7.468,17.516,5.641,15.69,15.8,5.539l1.827,1.826ZM18.4,6.566,13.932,2.1A3.175,3.175,0,0,1,18.4,6.566Z"
+                  transform="translate(-0.8 -0.255)"
+                  fill="#9d9d9d"
+                />
+              </svg>
+            </span>
+            <label className="text-secondary mb-0">Edit</label>
+          </Button>
+        </div>
+      </div>
+
       <div className="data-page user-profl-blc">
         <div className="d-flex user-profl-blc-rw">
           <div className="user-profl-col flex-fill">
@@ -53,7 +82,7 @@ const UserProfile = ({
                             edit={isEdit}
                           />
                           <div className="job-user-info flex-fill">
-                            <div className="edit-profile-blc">
+                            <div className="edit-profile-blc text-left">
                               <Label className="input-title">Email</Label>
                               <InputCell
                                 Name={"email"}
@@ -155,9 +184,21 @@ const UserProfile = ({
                 </LocalForm>
               </div>
               {!isEdit && (
-                <div className="user-profl-edit flex-shrink-0">
-                  {/* <Link className="btn btn-link" to={""}> */}
-                  <Button
+                <div className="flex-shrink-0">
+                  <div className="profile-bar ml-auto user-profile-bar">
+                    <span className="profile-percentage">95%</span>
+                    <div className="progress">
+                      <div
+                        className="progress-bar bg-primary"
+                        role="progressbar"
+                        style={{ width: "25%" }}
+                      ></div>
+                    </div>
+                    <span className="d-block profile-bar-txt text-left">
+                      Profile Completion
+                    </span>
+                  </div>
+                  {/* <Button
                     color="link"
                     className="edit-btn"
                     onClick={() => _toggleEdit(!isEdit)}
@@ -178,7 +219,7 @@ const UserProfile = ({
                       </svg>
                     </span>
                     <label className="text-secondary mb-0">Edit</label>
-                  </Button>
+                  </Button> */}
                   {/* </Link> */}
                 </div>
               )}
